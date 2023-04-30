51ae1e36b5a08ed61e2b35ce9d646d9c7f7aff92
GuilhermeFaga <faga@retize.com.br>
2023-04-30 18:27:29 -0300

    feat: lógica individual de gravação

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
 import './App.css';
+import Webcam from "react-webcam";
+import ReactPlayer from 'react-player'
+import { useCallback, useRef, useState } from 'react';
+import BottomNav from './components/BottomNav';
+import Card from './components/Card';
 
-function App() {
+
+export default function App() {
+  const [isRecording, setIsRecording] = useState(false);
+
+
+  const components = [
+    <VideoComponent />,
+    <VideoComponent />,
+  ]
+
+  var cl = components.length;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div
+        className={`video-grid bg-white dark:bg-gray-900 grid grid-flow-row grid-cols-1 md:grid-cols-${cl == 1 ? "1" : "2"} auto-rows-auto gap-2 p-2 h-full`}>
+        {components}
+      </div>
+      <BottomNav isRecording={isRecording} />
+    </>
   );
 }
 
-export default App;
+
+function VideoComponent({ }) {
+  const webcamRef = useRef(null);
+  const mediaRecorderRef = useRef(null);
+  const [capturing, setCapturing] = useState(false);
+  const [recordedChunks, setRecordedChunks] = useState([]);
+
+
+  const handleStartCaptureClick = useCallback(() => {
+    setCapturing(true);
+    setRecordedChunks([]);
+    mediaRecorderRef.current = new MediaRecorder(webcamRef.current.stream, {
+      mimeType: "video/webm"
+    });
+    mediaRecorderRef.current.addEventListener(
+      "dataavailable",
+      handleDataAvailable
+    );
+    mediaRecorderRef.current.start();
+  }, [webcamRef, setCapturing, mediaRecorderRef]);
+
+  const handleDataAvailable = useCallback(
+    ({ data }) => {
+      if (data.size > 0) {
+        setRecordedChunks((prev) => prev.concat(data));
+      }
+    },
+    [setRecordedChunks]
+  );
+
+  const handleStopCaptureClick = useCallback(() => {
+    mediaRecorderRef.current.stop();
+    setCapturing(false);
+  }, [mediaRecorderRef, webcamRef, setCapturing]);
+
+
+  return (
+
+    <div className='w-full flex items-center col-span-1 overflow-hidden'>
+      <div className='w-full h-fit'>
+        <Card>
+
+          <Webcam
+            className='w-full aspect-video'
+            ref={webcamRef}
+            audio={false} />
+
+          {/* {capturing ? (
+          <button onClick={handleStopCaptureClick} className='temp'>Stop Capture</button>
+          ) : (
+            <button onClick={handleStartCaptureClick} className='temp'>Start Capture</button>
+          )} */}
+
+          {recordedChunks.length > 0 && (
+            <ReactPlayer
+              className='absolute h-inherit w-inherit m-2 top-0 left-0'
+              height={webcamRef?.current.video.clientHeight}
+              width={webcamRef?.current.video.clientWidth}
+              url={URL.createObjectURL(new Blob(recordedChunks))} />
+          )}
+
+        </Card>
+      </div>
+    </div>
+  );
+
+}
---
+++ src/components/Card.js
@@ -0,0 +1,9 @@
+
+
+export default function Card({ children }) {
+  return (
+    <div class="h-fit w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      {children}
+    </div>
+  )
+}
